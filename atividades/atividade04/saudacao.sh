a8b179ab30f6fb638e0d4f952097b624b9bb72ff
André Sousa <andre.lipe.o@gmail.com>
2021-10-21 14:53:42 -0300

    Atividade 04 adicionada.

---
+++ atividades/atividade04/saudacao.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# primeira versão
+#echo "Olá $(whoami)," | tee -a saudacao.log
+#echo "$(date +'Hoje é dia %d, do mês %m do ano de %Y.')" | tee -a saudacao.log
+
+# segunda versão
+cat << EOF | tee -a saudacao.log
+Olá $(whoami),
+$(date +'Hoje é dia %d, do mês %m do ano de %Y.')
+EOF
