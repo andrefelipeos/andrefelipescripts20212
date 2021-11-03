8926d6a2c91474be2e2de566280cf5f535e87864
André Sousa <andre.lipe.o@gmail.com>
2021-11-03 16:39:43 -0300

    Script maiorDe3Verificado foi adicionado na atividade 05.

---
+++ atividades/atividade05/maiorDe3Verificado.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+regex="^[+-]?[0-9]+$"
+
+if ! [[ ${1} =~ ${regex} ]]
+then
+    echo "Opa!!! ${1} não é número."
+    exit 1
+fi
+
+if ! [[ ${2} =~ ${regex} ]]
+then
+    echo "Opa!!! ${2} não é número."
+    exit 1
+fi
+
+if ! [[ ${3} =~ ${regex} ]]
+then
+    echo "Opa!!! ${3} não é número."
+    exit 1
+fi
+
+maior=${1}
+
+if [ ${2} -gt ${maior} ]
+then
+    maior=${2}
+fi
+
+if [ ${3} -gt ${maior} ]
+then
+    maior=${3}
+fi
+
+echo ${maior}
