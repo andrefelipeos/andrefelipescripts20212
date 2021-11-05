0cddba79c420b90a73a2925ec45332a666218379
André Sousa <andre.lipe.o@gmail.com>
2021-11-05 14:15:35 -0300

    Script agenda.sh avisa que o arquivo está vazio caso ele não exista (na função listar) e que o e-mail passado como parâmetro não está cadastrado (na função remover).

--- atividades/atividade05/agenda.sh
+++ atividades/atividade05/agenda.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-comando=${1}
-nome=${2}
-email=${3}
 arquivo="agenda.db"
+
+comando=${1}
 
 if [ ${comando} == "adicionar" ]
 then
+    nome=${2}
+    email=${3}
+
     if [ ! -f ${arquivo} ]
     then
         echo "Arquivo criado!!!"
@@ -19,12 +21,28 @@ fi
 
 if [ ${comando} == "listar" ]
 then
+    if [ ! -f ${arquivo} ]
+    then
+        echo "Arquivo vazio!!!"
+	exit 2
+    fi
+
     cat ${arquivo}
 fi
 
 
 if [ ${comando} == "remover" ]
 then
-    sed -i /"${nome}"/d ${arquivo}
+    email=${2}
+
+    if [ $(grep ":${email}$" ${arquivo} | wc -l) -eq 0 ]
+    then
+        echo "E-mail não cadastrado."
+	exit 2
+    fi
+
+    nome=$(grep ":${email}$" ${arquivo} | cut -d":" -f1)
+
+    sed -i /":${email}$"/d ${arquivo}
     echo "Usuário ${nome} removido."
 fi
