8788adefe4f1fbc0ee7b10ab7fd563c26b3acb32
André Sousa <andre.lipe.o@gmail.com>
2021-11-06 15:37:17 -0300

    Script latencia.sh adicionado na atividade 06.

---
+++ atividades/atividade06/latencia.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARQUIVO=${1}
+TEMP="medias_ping"
+
+if [ -f ${TEMP} ]
+then
+    rm ${TEMP}
+fi
+
+for ip in $(cat ${ARQUIVO})
+do
+    echo "${ip} $(ping -c 10 ${ip} | tail --lines=1 | cut --delimiter="/" --fields=5)ms" >> ${TEMP}
+done
+
+cat ${TEMP} | sort --numeric-sort --key=2
+rm ${TEMP}
