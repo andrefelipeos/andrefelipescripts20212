aa3d6eacb0d30d4646563365f8a54a9a17a72454
André Sousa <andre.lipe.o@gmail.com>
2022-02-05 22:22:06 -0300

    MariaDB configurado para permitir conexões externas.

--- atividades/atividade15/user_data_1.sh
+++ atividades/atividade15/user_data_1.sh
@@ -15,21 +15,19 @@ systemctl enable mariadb.service
 
 # configura o mariadb para permitir conexoes externas
 # um alternativa é adicionar no arquivo /root/.my.cnf
-#cat << EOF >> /etc/my.cnf
-#[mysqld]
-#skip-networking=0
-#skip=bind-address
-#EOF
+cat << EOF >> /etc/my.cnf
+[mysqld]
+skip-networking=0
+skip-bind-address
+EOF
 
 # reinicia o servico mariadb para aplicar as configuracoes
-#systemctl restart mariadb.service
+systemctl restart mariadb.service
 
 
-# criar banco, tabela e usuario
+# cria o banco e o usuario
 mysql << EOF 2> /root/errors_mariadb_config.log
 CREATE DATABASE scripts;
 CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASSWORD}';
 GRANT ALL PRIVILEGES ON scripts.* TO '${DBUSER}'@'%';
-USE scripts;
-CREATE TABLE teste01 ( campo INT );
 EOF
