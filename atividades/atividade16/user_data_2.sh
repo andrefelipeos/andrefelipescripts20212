bd32e4c46bfd9f05b8c173b3d6a956a901078828
André Sousa <andre.lipe.o@gmail.com>
2022-02-12 23:30:46 -0300

    Atividade 16 incompleta

--- atividades/atividade16/user_data_2.sh
+++ atividades/atividade16/user_data_2.sh
@@ -3,31 +3,17 @@
 # devem ser modificadas pelo script principal!
 DBUSER=andre
 DBPASSWORD=senha
-IP_DBSERVER=172.31.89.2
+IP_DBSERVER=172.31.83.25
 
-# instala o cliente do mariadb.
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum update -y
-yum install -y mariadb
+yum install -y httpd mariadb wget
 
-# configura a senha para o usuário.
-# infelizmente não é suficiente para esse user_data.
-# mas funciona quando a maquina é acessada interativamente.
-cat << EOF > /root/.my.cnf
-[client]
-user=${DBUSER}
-password=${DBPASSWORD}
-EOF
+systemctl start httpd.service
+systemctl enable httpd.service
 
-# arquivo criado apenas para confirmar interativamente os parâmetros passados.
-cat << EOF > /root/parametros
-$(date)
-usuário: ${DBUSER}
-senha: ${DBPASSWORD}
-ip privado do servidor com banco de dados: ${IP_DBSERVER}
-EOF
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r ./wordpress/* /var/www/html/
 
-# só funcionou passando a senha explicitamente no comando!
-mysql -h ${IP_DBSERVER} -u ${DBUSER} -p${DBPASSWORD} scripts << EOF 2> /root/erros_conexao_mariadb.log
-USE scripts;
-CREATE TABLE Teste ( atividade INT );
-EOF
+systemctl restart httpd.service
